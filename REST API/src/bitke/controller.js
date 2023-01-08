3367afe7e97e1b7135b46dd6fa5b63f86a5c5d7a
LeonOfTheNap <95309331+LeonOfTheNap@users.noreply.github.com>
2023-01-08 03:35:58 +0100

    Add files via upload

--- REST API/src/bitke/controller.js
+++ REST API/src/bitke/controller.js
@@ -1,24 +1,41 @@
+const moment = require("moment/moment")
 const pool = require("../../db")
 const queries = require("./queries")
+var path = require("path")
+var fs = require('fs')
+
+function getStandardResponse(status, message, response){
+    return {
+        status: status,
+        message : message,
+        response : response
+     }
+}
 
 const getBitke = (req, res) => {
     pool.query(queries.getBitke, (error, results) => {
         if(error) throw error
-        res.status(200).json(results.rows)
+        res.status(200).json(getStandardResponse("OK", "Dohvat svih bitki", results.rows))
     })
 }
 
 const getBitkaById = (req, res) => {
     const id = (req.params.id)
-    pool.query(queries.getBitkaById, [id], (error, results) => {
+
+    if(!moment(id, "DD-MM-YYYY", true).isValid)
+        res.status(406).json(getStandardResponse("Not Acceptable", "Upišite pravilan datum.", null))
+
+    pool.query(queries.getBitkaById, [id], (error, results) => { try{
         if(error) throw error
         
         const nemaBitke = !(results.rows.length)
         if(nemaBitke) {
-            res.status(404).send("Unijeli ste datum za bitku koja ne postoji.")
+            res.status(404).json(getStandardResponse("Not Found", "Unijeli ste datum za bitku koja ne postoji.", null))
         }
-
-        res.status(200).json(results.rows)
+        else {res.status(200).json(getStandardResponse("OK", "Dohvat bitke.", results.rows))}
+        } catch (error) {
+            res.status(400).json(getStandardResponse("Bad Request","Niste upisali datum u obliku DD-MM-YYYY.", null))
+        }
     })
 }
 
@@ -26,15 +43,14 @@ const addBitka = (req, res) => {
     const { datum, pobjednik, sudionici, voditeljpobjednika, tijekomkojekoalicije, trajanjeudanima, ukupnoranjenihmrtvih, ime, lokacijaid} = req.body
 
     //provjeri datum postoji li ta bitka
-    pool.query(queries.provjeriDatum, [datum], (error, results) => {
+    pool.query(queries.provjeriDatum, [datum], (error, results) => { try{
         if(results.rows.length) {
-            res.send("Bitka za taj datum je već unesena.")
-        }
+            res.status(400).json(getStandardResponse("Bad Request", "Bitka za taj datum je već unesena.", null))}} catch (error) {res.status(400).json(getStandardResponse("Bad Request", "Niste upisali datum u obliku DD-MM-YYYY.", null))}
 
         //dodaja bitke
-        pool.query(queries.dodajBitku, [datum, pobjednik, sudionici, voditeljpobjednika, tijekomkojekoalicije, trajanjeudanima, ukupnoranjenihmrtvih, ime, lokacijaid], (error, results) =>{
+        pool.query(queries.dodajBitku, [datum, pobjednik, sudionici, voditeljpobjednika, tijekomkojekoalicije, trajanjeudanima, ukupnoranjenihmrtvih, ime, lokacijaid], (error, results) =>{ try{
             if(error) throw error
-            res.status(201).send("Bitka je dodana u bazu.")
+            res.status(201).json(getStandardResponse("Created", "Bitka je dodana u bazu.", results.rows))} catch (error) {res.status(400).json(getStandardResponse("Bad Request", "Niste upisali valjane podatke u odgovarajuća polja.", null))}
         })
     })
         
@@ -43,22 +59,72 @@ const addBitka = (req, res) => {
 const removeBitka = (req, res) =>{
     const id = (req.params.id)
     
-    pool.query(queries.getBitkaById, [id], (error, results) =>{
+    pool.query(queries.getBitkaById, [id], (error, results) =>{ try{
         const nemaBitke = !(results.rows.length)
         if(nemaBitke) {
-        res.send("Bitka s tim datumom ne postoji.")
-    }
+        res.status(400).json(getStandardResponse("Bad request", "Bitka s tim datumom ne postoji.", null))}} catch (error) {res.status(400).json(getStandardResponse("Bad Request", "Niste upisali datum u obliku DD-MM-YYYY.", null))}
+    
 
     pool.query(queries.removeBitka, [id], (error, results) =>{
         if(error) throw error
-        res.status(200).send("Bitka uspješno izbrisana iz baze.")
+        res.status(200).json(getStandardResponse("OK", "Bitka uspješno izbrisana iz baze.", null))
     })
     })
 }
+
+const getEgypt = (req, res) => {
+    pool.query(queries.getEgypt, (error, results) => {
+        if(error) throw error
+        res.status(200).json(getStandardResponse("OK", "Dohvat svih bitki u Egiptu", results.rows))
+    })
+}
+
+const getVictorNotFrance = (req, res) => {
+    pool.query(queries.getVictorNotFrance, (error, results) => {
+        if(error) throw error
+        res.status(200).json(getStandardResponse("OK", "Dohvat svih bitki u kojima pobjednik nije Francuska", results.rows))
+    })
+}
+
+const getViseDana = (req, res) => {
+    pool.query(queries.getViseDana, (error, results) => {
+        if(error) throw error
+        res.status(200).json(getStandardResponse("OK", "Dohvat svih bitki koje su trajale više dana.", results.rows))
+    })
+}
+
+const openapi = (req, res) => {
+    var data = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'openapi.json'), 'utf8'));
+    res.status(200).json(getStandardResponse("OK", "Dohvat OpenAPI dokumentacije", data))
+}
+
+const putBitkaById = (req, res) => {
+    const id = (req.params.id)
+    const { datum, pobjednik, sudionici, voditeljpobjednika, tijekomkojekoalicije, trajanjeudanima, ukupnoranjenihmrtvih, ime, lokacijaid} = req.body
+
+    //provjeri datum postoji li ta bitka
+    pool.query(queries.provjeriDatum, [id], (error, results) => { 
+        if(!results.rows.length) {
+            res.status(400).json(getStandardResponse("Bad Request", "Bitka za taj datum ne postoji.", null))}
+            
+        //azuriranje bitke
+        else(pool.query(queries.azurirajBitku, [datum, pobjednik, sudionici, voditeljpobjednika, tijekomkojekoalicije, trajanjeudanima, ukupnoranjenihmrtvih, ime, lokacijaid, id], (error, results) =>{ try{
+        if(error) throw error
+        res.status(200).json(getStandardResponse("OK", "Bitka je ažurirana u bazi.", results.rows))} catch (error) {res.status(400).json(getStandardResponse("Bad Request", "Niste upisali valjane podatke u odgovarajuća polja.", null))}
+    }))
+    })
+        
+}
+
 
 module.exports = {
     getBitke,
     getBitkaById,
     addBitka,
-    removeBitka
+    removeBitka,
+    getEgypt,
+    getVictorNotFrance,
+    getViseDana,
+    openapi,
+    putBitkaById
 }
--- REST API/src/bitke/routes.js
+++ REST API/src/bitke/routes.js
@@ -3,11 +3,16 @@ const controller = require("./controller")
 
 const router = Router()
 
-router.get('/',controller.getBitke)
-router.post('/', controller.addBitka)
+router.get('/', controller.getBitke)
 
-router.get('/:id', controller.getBitkaById)
-router.delete('/:id', controller.removeBitka)
+router.post('/jednabitka/', controller.addBitka)
+router.get('/jednabitka/:id', controller.getBitkaById)
+router.put('/jednabitka/:id', controller.putBitkaById)
+router.delete('/jednabitka/:id', controller.removeBitka)
 
+router.get('/getegypt', controller.getEgypt)
+router.get('/victornotfrance', controller.getVictorNotFrance)
+router.get('/visedana', controller.getViseDana)
+router.get('/openapi', controller.openapi)
 
 module.exports = router;
